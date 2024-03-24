4754d4e25c522f5db34bd9c8be8602ad2ddf3b5c
Bae Hyeonseo <haulqogustj@naver.com>
2024-03-24 20:37:17 +0900

    [chore]: 엔티티 생성

--- src/main/java/ussum/homepage/infra/jpa/comment/CommentMapper.java
+++ src/main/java/ussum/homepage/infra/jpa/comment/CommentMapper.java
@@ -1,4 +1,13 @@
 package ussum.homepage.infra.jpa.comment;
 
+import org.springframework.stereotype.Component;
+import ussum.homepage.domain.comment.PostComment;
+import ussum.homepage.domain.post.Post;
+
+@Component
 public class CommentMapper {
+    public PostComment toDomain(PostComment postComment){
+
+
+    }
 }
