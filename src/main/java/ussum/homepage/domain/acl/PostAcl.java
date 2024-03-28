2ba61a63fb2f1043612cdfea3aefdd87024c08cb
chahyunsoo <chahs1012@naver.com>
2024-03-29 03:12:48 +0900

    [refactor] #5 AclMapper 수정

--- src/main/java/ussum/homepage/domain/acl/PostAcl.java
+++ src/main/java/ussum/homepage/domain/acl/PostAcl.java
@@ -17,13 +17,12 @@ public class PostAcl {
     private String action;
     private String order;
     private Long postId;
-
     public static PostAcl of(Long id,
-                              Target target,
-                              Type type,
-                              Action action,
-                              Order order,
+                              String target,
+                              String type,
+                              String action,
+                              String order,
                               Long postId){
-        return new PostAcl(id, String.valueOf(target),String.valueOf(type), String.valueOf(action), String.valueOf(order), postId);
+        return new PostAcl(id, target,type, action, order, postId);
     }
 }
