b068c7e110e3fb8698d17bd1a06264f1ddbba179
Bae Hyeonseo <haulqogustj@naver.com>
2024-03-28 14:40:15 +0900

    [refactor] #8 url @RequestMapping 옆으로 변경

--- src/main/java/ussum/homepage/application/post/controller/BoardController.java
+++ src/main/java/ussum/homepage/application/post/controller/BoardController.java
@@ -2,6 +2,7 @@ package ussum.homepage.application.post.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ussum.homepage.application.post.service.dto.BoardListResponse;
@@ -9,13 +10,18 @@ import ussum.homepage.domain.post.service.BoardReader;
 import ussum.homepage.global.ApiResponse;
 
 @RequiredArgsConstructor
-@RequestMapping
+@RequestMapping("/boards")
 @RestController
 public class BoardController {
     private final BoardReader boardReader;
-    @GetMapping("/boards")
+    @GetMapping
     public ApiResponse<BoardListResponse> getBoardList() {
         BoardListResponse board = boardReader.getBoardList();
         return ApiResponse.onSuccess(board);
+    }
+    @PostMapping
+    public ApiResponse<?> createBoard() {
+
+        return ApiResponse.onSuccess(null);
     }
 }
