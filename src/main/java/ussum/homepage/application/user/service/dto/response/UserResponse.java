c7f29ce704f00a92b3e8713fff34a25317789821
chahyunsoo <chahs1012@naver.com>
2024-03-29 09:21:00 +0900

    [feat] #4 UserFormatter, UserResponse 임시 추가

---
+++ src/main/java/ussum/homepage/application/user/service/dto/response/UserResponse.java
@@ -0,0 +1,9 @@
+package ussum.homepage.application.user.service.dto.response;
+
+public record UserResponse(
+
+) {
+    public static UserResponse of() {
+
+    }
+}
---
+++ src/main/java/ussum/homepage/domain/user/service/formatter/UserFormatter/UserFormatter.java
@@ -0,0 +1,9 @@
+package ussum.homepage.domain.user.service.formatter.UserFormatter;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserFormatter {
+}
