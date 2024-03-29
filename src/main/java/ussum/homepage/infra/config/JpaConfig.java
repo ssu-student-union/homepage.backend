4ead6574f7ce1db32bf7da847dc60fee68576f2f
Bae Hyeonseo <haulqogustj@naver.com>
2024-03-29 20:30:19 +0900

    [fix] #8 additional data 삭제 및 jpaconig 추가

---
+++ src/main/java/ussum/homepage/infra/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package ussum.homepage.infra.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+}
+
